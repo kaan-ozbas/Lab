27cdd1a947ab3caa8a2d9627ce9ef31760dff54e
kaan-ozbas <kaan.ozbas@ug.bilkent.edu.tr>
2021-10-20 18:02:03 +0300

    implement findOne method

--- src/StudentSearch.java
+++ src/StudentSearch.java
@@ -9,6 +9,12 @@ public class StudentSearch {
             if (student.getId().equals(id))
                 return true;
 
-        return false;
+			return false;
     }
+}
+public Student findOne(ArrayList<Student> students, String name) throws Exception {
+for (Student student: students)
+if (student.getName().equals(name))
+return student;
+throw new Exception("There is no student with the given name!");
 }
